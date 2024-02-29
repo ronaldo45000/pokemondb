35aa280a2e807d4388051b81986d6c7612729723
onme7 <Onme7052@gmail.com>
2024-02-28 16:39:36 -0800

    first

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const mysql = require('mysql');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 4000;
+app.use("/assets",express.static("assets"));
+
+// app.use(express.static(__dirname));
+
+// MySQL connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root1234',
+    database: 'register',
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+});
+
+// Middleware //NEED THIS ONE 
+app.use(session({
+    secret: 'secret_key',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//for actually reading the string
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+// send to homepage 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/loginTest.html');
+});
+app.use(express.static(__dirname));
+//Post your register data into database
+
+//communicate to server
+app.post('/register', (req, res) => {
+    const { username, password } = req.body
+    const sql = 'INSERT INTO reg (username, password) VALUES (?, ?)';
+    db.query(sql, [username, password], (err) => {
+        if (err) throw err;
+        res.sendFile(__dirname + '/loginTest.html')
+    });
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const sql = 'SELECT * FROM reg WHERE username = ? AND password = ?';
+    db.query(sql, [username, password], (err, results) => {
+        if (err) throw err;
+        if (results.length > 0) {
+            req.session.loggedin = true;
+            req.session.username = username;
+                        //just give a string name (like a address to get in)
+         //   res.redirect('/navB.html');
+            res.sendFile(__dirname + '/navB.html');
+        } else {
+         res.sendFile(__dirname + '/loginTest.html');
+          }
+    });
+});
+
+// Server start
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ first.js
@@ -0,0 +1,20 @@
+const loginButton = document.getElementById("Start");
+
+
+// Add a click event listener to the login button
+loginButton.addEventListener("click", function() {
+    // Redirect to the new HTML page (dashboard.html)
+    event.preventDefault(); // Prevent the default form submission.
+
+    // Get the entered username and password.
+
+    // Check if the entered credentials are correct.
+   
+        // If correct, navigate to the "hello.html" page.
+        window.location.href = "resume.html";
+   
+        // If incorrect, display an error message (you can customize this part).
+     
+    
+});
+
