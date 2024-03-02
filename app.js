def227360338ad604c8446ffba4c5cbcad98c4fa
onme7 <Onme7052@gmail.com>
2024-03-02 10:56:21 -0800

    updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 5500;
+const port = 4000;
 app.use("/assets",express.static("assets"));
 
 // app.use(express.static(__dirname));
@@ -14,7 +14,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root1234',
-    database: 'register',
+    database: 'bennett_riley_le_thinh_db',
 });
 
 db.connect((err) => {
@@ -68,6 +68,92 @@ app.post('/login', (req, res) => {
           }
     });
 });
+
+app.get('/search', (request, response) => {
+    //q here from index.html
+const query = request.query.q;
+var sql = '';
+
+
+if(query != 'info'&&query!= 'caughtpokemon' &&query!= 'moves' &&query!="typechart")
+	{
+        if(query == 'user'){
+            sql = `SELECT * FROM reg ORDER BY user_id`;
+
+        }
+else{
+		sql = `SELECT * FROM reg WHERE username LIKE '%${query}%'  OR user_id LIKE '%${query}%'`;
+}
+    }
+
+    else if(query == 'info'){
+        sql = `SELECT * FROM info ORDER BY fname`;
+
+    }
+    else if(query == 'caughtpokemon'){
+        sql = `SELECT * FROM caughtpokemon ORDER BY PokedexId`;
+
+    }
+    else if(query == 'moves'){
+        sql = `SELECT * FROM moves ORDER BY Move_Name`;
+
+    }
+    else if(query == 'typechart'){
+        sql = `SELECT * FROM typechart ORDER BY Attacking_Type`;
+
+    }
+    //make sure to hand empt
+	else{
+// sql = `SELECT * FROM reg ORDER BY user_id`;
+sql = `SELECT * FROM reg ORDER BY user_id`;
+    }
+
+db.query(sql, (error, results) => {
+
+if (error) throw error;
+
+response.send(results);
+
+});
+
+});
+app.post("/delete_data", (request, response) => {
+
+	const id = request.body.id;
+    const fname = request.body.fname;
+
+    const query = request.body.q;
+console.log("query is " + fname)
+	const sql = `DELETE FROM reg WHERE user_id = '${id}'`;
+	const sql2 = `DELETE FROM info WHERE fname = '${fname}'`;
+    if(id){
+	db.query(sql, (error, results) => {
+		response.json({
+			message : 'Data Deleted'
+		});
+	});
+}
+else if(fname){
+    db.query(sql2, (error, results) => {
+		response.json({
+			message : 'Data Deleted'
+		});
+	});
+}
+});
+// app.post("/delete_data", (request, response) => {
+
+// 	const id = request.body.id;
+
+// 	const sql2 = `DELETE FROM info WHERE fname = '${id}'`;
+
+
+//     db.query(sql2, (error, results) => {
+// 		response.json({
+// 			message : 'Data Deleted'
+// 		});
+// 	});
+// });
 
 // Server start
 app.listen(port, () => {
