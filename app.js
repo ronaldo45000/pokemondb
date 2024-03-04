0cb9b21c169c5bdfeaa31a79a80bb2d241cc1295
onme7 <Onme7052@gmail.com>
2024-03-04 11:40:10 -0800

    insert,del,update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 4000;
+const port = 5000;
 app.use("/assets",express.static("assets"));
 
 // app.use(express.static(__dirname));
@@ -14,7 +14,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root1234',
-    database: 'bennett_riley_le_thinh_db',
+    database: 'project',
 });
 
 db.connect((err) => {
@@ -51,7 +51,105 @@ app.post('/register', (req, res) => {
         res.sendFile(__dirname + '/loginTest.html')
     });
 });
+app.post('/insert', (req, res) => {
+    const { pokemon1, pokemon2,pokemon3,pokemon4,pokemon5,pokemon6,pokemon7,pokemon8 } = req.body
+    const sql = 'INSERT INTO caughtpokemon (PokedexId, TrainerId,Pokemon_Species,Pokemon_Level,Ability,Nature,Nickname,Held_Item) VALUES (?, ?,?, ?,?, ?,?, ?)';
+    db.query(sql, [pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6, pokemon7,pokemon8], (err) => {
+        if (err) {
+            // Check for constraint violation error
+            if (err.code === 'ER_DUP_ENTRY') {
+                return res.status(400).json({ error: 'Duplicate entry. The move may already exist.' });
+            } else if (err.code === 'SOME_OTHER_ERROR_CODE') {
+                // Handle other constraint violation errors here
+                return res.status(400).json({ error: 'Some other constraint violation error.' });
+            } else {
+                // Handle other types of errors
+                return res.status(500).json({ error: 'Internal Server Error.' });
+            }
+        }
 
+        // Successful insertion
+        return res.status(200).json({ success: true, message: 'Move inserted successfully.' });
+    });
+});
+app.post('/insert2', (req, res) => {
+    const {fname,lname } = req.body
+    const sql = 'INSERT INTO info (fname, lname) VALUES (?, ?)';
+    db.query(sql, [ fname, lname], (err) => {
+        if (err) throw err;
+      
+    });
+});
+
+app.post('/insert3', (req, res) => {
+    const { moves1, moves2, moves3, moves4, moves5, moves6, moves7 } = req.body;
+
+    const sql = 'INSERT INTO moves (Move_Name, Move_Type, Category, Power, Accuracy, PP, Move_Description) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    db.query(sql, [moves1, moves2, moves3, moves4, moves5, moves6, moves7], (err) => {
+        if (err) {
+            // Check for constraint violation error
+            if (err.code === 'ER_DUP_ENTRY') {
+                return res.status(400).json({ error: 'Duplicate entry. The move may already exist.' });
+            } else if (err.code === 'SOME_OTHER_ERROR_CODE') {
+                // Handle other constraint violation errors here
+                return res.status(400).json({ error: 'Some other constraint violation error.' });
+            } else {
+                // Handle other types of errors
+                return res.status(500).json({ error: 'Internal Server Error.' });
+            }
+        }
+
+        // Successful insertion
+        return res.status(200).json({ success: true, message: 'Move inserted successfully.' });
+    });
+});
+
+
+app.post('/updateData', (req, res) => {
+    const { username, password } = req.body;
+    const sql = 'UPDATE reg SET password = ? WHERE username = ?';
+    db.query(sql, [password, username], (err, result) => {
+        if (err) {
+            console.error('Error updating data:', err);
+            res.json({ success: false, message: 'Error updating data in the database.' });
+        } else {
+            console.log('Number of records updated:', result.affectedRows);
+            res.json({ success: true, message: 'Data updated successfully.' });
+        }
+    });
+});
+app.post('/updateData2', (req, res) => {
+    //be careful this get data base on order how you put, when query make sure it is in correct order (Update might mess order)
+    const { moves1, moves2, moves3, moves4, moves5, moves6, moves7 } = req.body;
+ 
+    const sql2 = 'UPDATE moves SET Power = ?, Accuracy = ?, PP = ?, Move_Description = ? WHERE Move_Name = ? AND Move_Type = ? AND Category = ?';
+
+    db.query(sql2, [moves4, moves5, moves6, moves7, moves1, moves2, moves3], (err, result) => {
+        if (err) {
+            console.error('Error updating data:', err);
+            res.json({ success: false, message: 'Error updating data in the database.' });
+        } else {
+            console.log('Number of records updated:', result.affectedRows);
+            res.json({ success: true, message: 'Data updated successfully.' });
+        }
+    });
+});
+app.post('/updateData3', (req, res) => {
+    //be careful this get data base on order how you put, when query make sure it is in correct order (Update might mess order)
+    const { pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6, pokemon7,pokemon8 } = req.body;
+ 
+    const sql2 = 'UPDATE caughtpokemon SET Pokemon_Species = ?, Pokemon_Level = ?, Ability = ?, Nature = ?,Nickname = ?, Held_Item = ? WHERE PokedexId = ? AND TrainerId = ?';
+
+    db.query(sql2, [pokemon3, pokemon4, pokemon5, pokemon6, pokemon7, pokemon8, pokemon1,pokemon2], (err, result) => {
+        if (err) {
+            console.error('Error updating data:', err);
+            res.json({ success: false, message: 'Error updating data in the database.' });
+        } else {
+            console.log('Number of records updated:', result.affectedRows);
+            res.json({ success: true, message: 'Data updated successfully.' });
+        }
+    });
+});
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
     const sql = 'SELECT * FROM reg WHERE username = ? AND password = ?';
@@ -75,20 +173,19 @@ const query = request.query.q;
 var sql = '';
 
 
-if(query != 'info'&&query!= 'caughtpokemon' &&query!= 'moves' &&query!="typechart")
-	{
+// if(query != 'info'&&query!= 'caughtpokemon' &&query!= 'moves' &&query!="typechart"&&query!= 'pokemon')
+// 	{
         if(query == 'user'){
             sql = `SELECT * FROM reg ORDER BY user_id`;
 
         }
-else{
-		sql = `SELECT * FROM reg WHERE username LIKE '%${query}%'  OR user_id LIKE '%${query}%'`;
-}
-    }
+// else{
+// 		sql = `SELECT * FROM reg WHERE username LIKE '%${query}%'  OR user_id LIKE '%${query}%'`;
+// }
+    //}
 
     else if(query == 'info'){
         sql = `SELECT * FROM info ORDER BY fname`;
-
     }
     else if(query == 'caughtpokemon'){
         sql = `SELECT * FROM caughtpokemon ORDER BY PokedexId`;
@@ -102,10 +199,14 @@ else{
         sql = `SELECT * FROM typechart ORDER BY Attacking_Type`;
 
     }
+    else if (query == 'pokemon'){
+        sql = `SELECT * FROM pokemon ORDER BY PokedexId`;
+    }
     //make sure to hand empt
 	else{
 // sql = `SELECT * FROM reg ORDER BY user_id`;
-sql = `SELECT * FROM reg ORDER BY user_id`;
+sql = `SELECT * FROM typechart ORDER BY Attacking_Type`;
+
     }
 
 db.query(sql, (error, results) => {
@@ -121,11 +222,16 @@ app.post("/delete_data", (request, response) => {
 
 	const id = request.body.id;
     const fname = request.body.fname;
+    const caughtPokemon = request.body.caughtPokedex;
+    const move = request.body.Move_Name;
 
-    const query = request.body.q;
-console.log("query is " + fname)
+
 	const sql = `DELETE FROM reg WHERE user_id = '${id}'`;
 	const sql2 = `DELETE FROM info WHERE fname = '${fname}'`;
+    const sql3 = `DELETE FROM caughtpokemon WHERE PokedexId = '${caughtPokemon}'`;
+    const sql4 = `DELETE FROM moves WHERE Move_Name = '${move}'`;
+
+    // DELETE FROM CaughtPokemon WHERE TrainerId = 3 AND PokemonId = 654
     if(id){
 	db.query(sql, (error, results) => {
 		response.json({
@@ -138,9 +244,38 @@ else if(fname){
 		response.json({
 			message : 'Data Deleted'
 		});
+       
 	});
+
+
 }
+else if (caughtPokemon) {
+        db.query(sql3, (error, results) => {
+            if (error) {
+                console.error("Failed to delete data:", error);
+                return response.status(500).json({ error: "Failed to delete data." });
+            }
+
+            response.json({
+                message: "Data Deleted"
+            });
+        });
+    }
+    else if (move) {
+        db.query(sql4, (error, results) => {
+            if (error) {
+                console.error("Failed to delete data:", error);
+                return response.status(500).json({ error: "Failed to delete data." });
+            }
+
+            response.json({
+                message: "Data Deleted"
+            });
+        });
+    }
 });
+
+
 // app.post("/delete_data", (request, response) => {
 
 // 	const id = request.body.id;
